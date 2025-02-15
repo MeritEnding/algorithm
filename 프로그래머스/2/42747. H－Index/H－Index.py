40550e264709716bcd594b2a45293e4b3ae21176
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-15 23:39:34 +0900

    [level 2] Title: H-Index, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,9 @@
 def solution(citations):
-    answer = 0
+    answer=0
     citations.sort(reverse=True)
-    
-    for i in range(len(citations)):
+
+    for i in range(0,len(citations)):
         if citations[i]<i+1:
             return i
+    
     return len(citations)
