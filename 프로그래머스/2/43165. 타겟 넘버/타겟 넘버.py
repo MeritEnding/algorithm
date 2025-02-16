65eaeb51eb220eece2c01b2f2d761b9a11a27257
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-16 19:59:53 +0900

    [level 2] Title: 타겟 넘버, Time: 251.00 ms, Memory: 49.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,15 @@
 def solution(numbers, target):
-    answer = 0
-    leaves = [0]  # 초기값 0에서 시작
-    for num in numbers:
-        tmp = []
-        for parent in leaves:  # 올바른 변수명 사용
-            tmp.append(parent + num)
-            tmp.append(parent - num)
-        leaves = tmp  # "leves"가 아닌 "leaves" 사용
-    for leaf in leaves:
-        if leaf == target:
-            answer += 1
-    return answer
+    leavse=[0]
+
+    for i in numbers:
+        tmp =[]
+        for j in leavse:
+            tmp.append(j+i)
+            tmp.append(j-i)
+        leavse=tmp
+    
+    result=0
+    for i in leavse:
+        if i ==target:
+            result+=1
+    return result
