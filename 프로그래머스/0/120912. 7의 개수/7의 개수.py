31d11764899dab52c39e6b6583a895df1d2f44f6
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-17 11:26:36 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -0,0 +1,8 @@
+def solution(array):
+    answer = 0
+    for i in range(len(array)):
+        for j in str(array[i]):
+            if j =='7':
+                answer+=1
+                        
+    return answer
