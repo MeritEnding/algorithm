1336f299c194812aea9cd2cdfbe600b7fa72891a
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-18 23:30:42 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
@@ -1,9 +1,7 @@
 def solution(arr):
-    answer =0
-    for i in range(len(arr)):
-        answer += arr[i]
+  
     
-    answer=answer/len(arr)
     
+    answer = sum(arr)/len(arr)
     
     return answer
