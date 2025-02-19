0b44b3a3412e327e33cdd4e20a350dc2d86d61e7
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-19 14:11:41 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,11 @@
 def solution(clothes):
-    # 옷을 종류별로 구분하기
-    hash_map ={}
-    for clothe, type in clothes:
-        hash_map[type]= hash_map.get(type, 0)+1
+    hash_map={}
 
-    # 입지 않은 경우를 추가하여 모든 조합 계산하기
+    for clothe, type in clothes:
+        hash_map[type]= hash_map.get(type,0)+1
+
     answer=1
     for type in hash_map:
-        answer *=(hash_map[type]+1)
-
+        answer *= (hash_map[type]+1)
+    
     return answer -1
