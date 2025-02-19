5605660bd81452725acaa7dcfbd2382b61fecbce
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-19 19:15:05 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,8 @@
 def solution(array,commands):
-    ans =[]
-    answer=[]
+    ans=[]
+    answer =[]
     for i in commands:
-        ans = array[i[0]-1:i[1]]
+        ans =array[i[0]-1:i[1]]
         ans.sort()
         answer.append(ans[i[2]-1])
-
     return answer
