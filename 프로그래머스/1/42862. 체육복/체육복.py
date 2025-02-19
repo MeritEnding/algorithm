ba14de77cfa01345feaf6d082637024644aff744
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-19 12:33:46 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,8 +1,8 @@
 def solution(n,lost,reserve):
-    net_lost = set(lost)-set(reserve)
-    net_reserve=set(reserve)-set(lost)
+    net_lost =set(lost)-set(reserve)
+    net_reserve= set(reserve)-set(lost)
 
-    answer = n -len(net_lost)
+    answer =n-len(net_lost)
 
     for i in sorted(net_lost):
         if i-1 in net_reserve:
@@ -12,4 +12,3 @@ def solution(n,lost,reserve):
             net_reserve.remove(i+1)
             answer+=1
     return answer
-
