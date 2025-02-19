a3b1f21beed8529dd39331a3b9aa84cbcf116a00
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-19 11:58:28 +0900

    [level 2] Title: 소수 찾기, Time: 6.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -4,19 +4,22 @@ from itertools import permutations
 def is_prime(n):
     if n<=1:
         return False
+    
     for i in range(2, int(math.sqrt(n))+1):
-        if n% i==0:
+        if n%i==0:
             return False
     return True
+
 def solution(numbers):
-    unique_numbers=set()
+    unique=set()
 
-    for i in range(1, len(numbers)+1):
-        perms=permutations(numbers,i)
+    for i in range(1,len(numbers)+1):
+        perms =permutations(numbers,i)
+
         for perm in perms:
-            num=int(''.join(perm))
-            unique_numbers.add(num)
+            num = int(''.join(perm))
+            unique.add(num)
 
-    prime_count =sum(1 for num in unique_numbers if is_prime(num))
+    prime_count =sum(1 for i in unique if is_prime(i))
 
     return prime_count
