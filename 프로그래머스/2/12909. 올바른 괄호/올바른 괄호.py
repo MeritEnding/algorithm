1c63a8e579e85ff9439e7be1d7816175573f60d5
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 16:02:02 +0900

    [level 2] Title: 올바른 괄호, Time: 7.60 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,15 @@
 def solution(s):
-    answer =True
-    stack =[]
+    answer = True
+    stack=[]
+    
     for i in s:
-        if i =='(':
+        if i=='(':
             stack.append('(')
         else:
-            if stack==[]:
+            if stack ==[]:
                 return False
             else:
                 stack.pop()
-    if stack != []:
+    if stack !=[]:
         return False
-
     return True
