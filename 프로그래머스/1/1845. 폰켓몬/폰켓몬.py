3b1c34c00ce38271127dcdeb2e85350ce1af6e70
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 15:42:07 +0900

    [level 1] Title: 폰켓몬, Time: 1.25 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,9 @@
 def solution(nums):
     n_dict={}
     
-    for i in nums:
-        n_dict[i]=1
-    
+    for num in nums:
+        n_dict[num]=1
+        
     if len(nums)//2 < len(n_dict):
         return len(nums)//2
     else:
