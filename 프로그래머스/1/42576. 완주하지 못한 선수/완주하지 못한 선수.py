c989cd39ee4f9a4f390bfccac8e178a922e59280
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 12:40:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.26 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -4,9 +4,7 @@ def solution(participant, completion):
     for part in participant:
         hash_map[hash(part)]=part
         sum+=hash(part)
-    
     for comp in completion:
         sum-=hash(comp)
-
-    return hash_map[sum]
     
+    return hash_map[sum]
