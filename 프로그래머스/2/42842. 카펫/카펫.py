45ef9f00058512fe336162fd7a91ea1c2636bcdd
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 14:44:32 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -3,11 +3,12 @@ def solution(brown, yellow):
     yellow_y=0
     answer=[]
     
-    for i in range(1,yellow+1):
+    for i in range(1, yellow+1):
         if yellow % i ==0:
-            yellow_x = yellow//i
-            yellow_y = i
-            if 2*yellow_x + 2*yellow_y+4 ==brown:
+            yellow_x =yellow//i
+            yellow_y= i
+            
+            if 2*yellow_x+ 2*yellow_y+4 ==brown:
                 answer.append(yellow_x+2)
                 answer.append(yellow_y+2)
                 break
