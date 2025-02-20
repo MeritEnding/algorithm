33284d1fdf6f760f02c7bd16d8f773fb717a63ab
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 16:53:36 +0900

    [level 2] Title: 타겟 넘버, Time: 202.72 ms, Memory: 49.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -3,14 +3,13 @@ def solution(numbers, target):
     result=0
     
     for i in numbers:
-        tmp =[]
+        tmp=[]
         for j in leaves:
             tmp.append(j+i)
             tmp.append(j-i)
         leaves=tmp
-    
-    for i in leaves:
-        if i ==target:
+    for j in leaves:
+        if j ==target:
             result+=1
-            
+
     return result
