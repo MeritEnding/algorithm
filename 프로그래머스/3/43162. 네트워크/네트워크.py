f332c02cca2892516a14036f458e38db4132b35a
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 16:59:20 +0900

    [level 3] Title: 네트워크, Time: 0.65 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,17 @@
-def dfs(n,computers,v, visited):
+def dfs(n, computers, v, visited):
     visited[v]=True
     
     for j in range(n):
-        if j!=v and computers[v][j]==1:
+        if j != v and computers[v][j]==1:
             if not visited[j]:
-                dfs(n,computers,j,visited)
-    
+                dfs(n,computers,j, visited)
 
+                
 def solution(n,computers):
     visited=[False]*n
-    result =0
+    result = 0
     for i in range(n):
         if not visited[i]:
-            dfs(n,computers, i ,visited)
-            result+=1
-            
+            dfs(n, computers, i, visited)
+            result += 1
     return result
