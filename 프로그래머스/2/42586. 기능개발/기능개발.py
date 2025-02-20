169cdae92e8c4b038cd246223b018fc7c08e3d03
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 17:22:03 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,24 @@
 from collections import deque
 import math
 def solution(progresses, speeds):
-    progresses=deque(progresses)
-    speeds=deque(speeds)
+    progresses= deque(progresses)
+    speeds =deque(speeds)
     answer =[]
     
     while progresses:
-        day= math.ceil((100-progresses[0])/speeds[0])
+        day= math.ceil((100- progresses[0])/speeds[0])
         
         for i in range(len(progresses)):
-            progresses[i]+=day*speeds[i]
-        
+            progresses[i] += day*speeds[i]
+            
+            
         release=0
         while progresses and progresses[0]>=100:
+            
             release+=1
             progresses.popleft()
             speeds.popleft()
+            
         answer.append(release)
         
     return answer
