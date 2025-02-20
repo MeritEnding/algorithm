fd4a261ba123424453ea0e634110ff7d34314d3e
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 17:27:38 +0900

    [level 2] Title: 프로세스, Time: 3.61 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,14 +1,15 @@
 from collections import deque
 def solution(priorities, location):
-    queue=deque()
-    answer=0
+    answer =0
+    queue= deque()
     
     for i in range(len(priorities)):
         queue.append((priorities[i],i))
         
     while queue:
-        cur = queue.popleft()
-        max_val =max([i[0] for i in queue], default=0)
+        cur =queue.popleft()
+        max_val =max([i[0] for i in queue],default=0)
+        
         if cur[0] <max_val:
             queue.append(cur)
         else:
