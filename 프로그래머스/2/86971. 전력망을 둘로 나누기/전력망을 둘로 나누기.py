902609f1ef439fd458a4f8a3e19d9a2214c63855
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 23:50:36 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.25 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,15 +1,14 @@
 from collections import deque
 def bfs(start, tree, visited, wire):
+    
     queue =deque([start])
     visited[start]=True
-    
-    cnt =1 
-    
+    cnt=1
     while queue:
         now =queue.popleft()
         
         for neighbor in tree[now]:
-            if (now ==wire[0] and neighbor ==wire[1]) or (now == wire[1] and neighbor ==wire[0]):
+            if (now == wire[0] and neighbor == wire[1]) or (now == wire[1] and neighbor == wire[0]):
                 continue
             if not visited[neighbor]:
                 queue.append(neighbor)
@@ -18,18 +17,19 @@ def bfs(start, tree, visited, wire):
     return cnt
     
 
+
 def solution(n, wires):
     answer = float("inf")
-    tree=[[] for _ in range(n+1)]
+    tree =[[] for _ in range(n+1)]
     
-    for a, b in wires:
+    for a,b in wires:
         tree[a].append(b)
         tree[b].append(a)
         
     for wire in wires:
         visited=[False]*(n+1)
-        cnt1= bfs(wire[0], tree, visited, wire)
-        cnt2 = n- cnt1
-        answer =min(answer , abs(cnt1-cnt2))
+        cnt1 = bfs(wire[0], tree, visited, wire)
+        cnt2 = n -cnt1
+        answer = min(answer, abs(cnt1-cnt2))
     return answer
-        
+    
