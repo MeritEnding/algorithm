4dc18a96e331d90b2085fd54fca1d9d1754d46ac
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-20 14:29:42 +0900

    [level 1] Title: 모의고사, Time: 3.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -5,16 +5,18 @@ def solution(answers):
     student3=[3,3,1,1,2,2,4,4,5,5]
     
     score=[0,0,0]
+    
     for i in range(len(answers)):
-        if answers[i] == student1[i%5]:
+        if answers[i]== student1[i%5]:
             score[0]+=1
-        if answers[i] == student2[i%8]:
+        if answers[i]== student2[i%8]:
             score[1]+=1
-        if answers[i] == student3[i%10]:
+        if answers[i]==student3[i%10]:
             score[2]+=1
-            
+    
     for idx, value in enumerate(score):
         if value == max(score):
             result.append(idx+1)
+            
     return result
-        
+    
