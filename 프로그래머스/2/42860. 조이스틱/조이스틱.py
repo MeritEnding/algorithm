4379c5570bb68ceccd3d95162311accd49876357
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 14:14:00 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,17 +1,17 @@
 def solution(name):
-    
+    answer = 0
     spell_move =0
-    cursor_move = len(name)-1
+    cursor_move =len(name)-1
     
     for i , spell in enumerate(name):
-        spell_move += min(ord(spell)-ord('A'), ord('Z')-ord(spell)+1)
+        spell_move += min(ord(spell)-ord('A'),ord('Z')-ord(spell)+1)
         
-        next =i+1
+        next = i+1
         
-        while next< len(name) and name[next] =='A':
+        while next <len(name) and name[next]=='A':
             next+=1
-        
-        cursor_move =min([cursor_move, i*2+ len(name)-next, i+2*(len(name)-next)])
+            
+        cursor_move = min([cursor_move , 2*i+len(name)-next, i+2*(len(name)-next)])
         
     return cursor_move + spell_move
     
