df4030c0e7c7d970468fecb0c881b7cdefceaa6f
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 13:58:29 +0900

    [level 2] Title: 피로도, Time: 30.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -3,14 +3,12 @@ def solution(k, dungeons):
     answer = 0
     
     for p in permutations(dungeons, len(dungeons)):
-        
-        tmp =k 
-        cnt =0 
+        tmp = k
+        cnt = 0
         
         for need, spend in p:
             if tmp >=need:
-                tmp -=spend
-                cnt+=1
+                tmp -= spend
+                cnt += 1
         answer = max(answer, cnt)
-                
     return answer
