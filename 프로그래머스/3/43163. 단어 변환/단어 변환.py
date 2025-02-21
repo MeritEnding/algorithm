ca8cb4fd5933f3d919e280881090240f16f8bd6e
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 14:27:58 +0900

    [level 3] Title: 단어 변환, Time: 6.67 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -3,25 +3,25 @@ def solution(begin, target, words):
     if target not in words:
         return 0
     else:
-        return bfs(begin,target,words)
+        return bfs(begin, target, words)
+
+def bfs(begin, target, words):
+    queue =deque()
+    queue.append([begin, 0])
     
-def bfs(begin,target,words):
-    queue= deque()
-    queue.append([begin,0])
     
     while queue:
-        now ,step= queue.popleft()
+        now , step=queue.popleft()
         
-        if now ==target:
+        if now == target:
             return step
         
         for word in words:
             count =0
             for i in range(len(now)):
-                if word[i] != now[i]:
+                if now[i] != word[i]:
                     count+=1
-            
             if count ==1:
                 queue.append([word, step+1])
-            
+        
     
