317fc58a88ec2a448afc2ba7418ec4dba842269a
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 18:06:57 +0900

    [level 2] Title: 주식가격, Time: 70.28 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,14 @@
 from collections import deque
-
 def solution(prices):
     answer = []
+    
     price_q =deque(prices)
     
     while price_q:
-        cur_price=price_q.popleft()
+        cur_price =price_q.popleft()
         time=0
         for p in price_q:
-            time+=1
+            time +=1
             if cur_price > p:
                 break
         answer.append(time)
