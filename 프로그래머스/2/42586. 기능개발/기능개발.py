f5f67eafa9d3e69a592378f827cc9554305da2df
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 19:33:59 +0900

    [level 2] Title: 기능개발, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -7,17 +7,12 @@ def solution(progresses, speeds):
     
     while progresses:
         day = math.ceil((100-progresses[0])/speeds[0])
-        
         for i in range(len(progresses)):
-            progresses[i] +=speeds[i]*day
-            
+            progresses[i] += day*speeds[i]
         release=0
-        while progresses and progresses[0]>=100:
-            release += 1
+        while progresses and progresses[0] >=100:
+            release+=1
             progresses.popleft()
             speeds.popleft()
         answer.append(release)
-    return answer
-            
-        
     return answer
