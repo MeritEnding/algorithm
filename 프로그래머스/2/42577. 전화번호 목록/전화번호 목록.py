7d3967370211b76e87711478deb96657ca87b0d3
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 13:23:00 +0900

    [level 2] Title: 전화번호 목록, Time: 439.40 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,16 +1,16 @@
 def solution(phone_book):
     answer = True
-    
-    hash_map = {}
+    hash_map={}
     for nums in phone_book:
         hash_map[nums]=1
         
     for nums in phone_book:
-        answer = ''
+        answer=''
         for num in nums:
-            answer += num
-            if answer in hash_map and answer != nums:
+            answer+=num
+            
+            if answer in hash_map and answer!=nums:
                 return False
     return True
     
-  
+    return answer
