5d9744a5cc282eefde80cc32bdfeec6938b8ee69
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 15:09:44 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,9 +2,12 @@ def solution(clothes):
     answer = 1
     hash_map={}
     for clothe, type in clothes:
-        hash_map[type]=hash_map.get(type,0)+1
+        hash_map[type] =hash_map.get(type,0)+1
     
     for type in hash_map:
         answer *= (hash_map[type]+1)
     
-    return answer -1
+    return answer - 1
+        
+        
+    return answer
