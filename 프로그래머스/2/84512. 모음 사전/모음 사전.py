5f994ae467703c49a38dc9333a52b12c81bd3732
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-21 14:09:35 +0900

    [level 2] Title: 모음 사전, Time: 1.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,15 +1,18 @@
 from itertools import product
+
 def solution(word):
     answer = 0
     num=1
-    alpha=['A','E','I','O','U']
     dictionary=[]
-    for i in range(5):
-        for p in product(alpha,repeat=num):
-            dictionary.append(''.join(p))
-        num+=1
-    dictionary.sort()
-    answer = dictionary.index(word)+1
+    alpha=['A','E','I','O','U']
     
-            
+    
+    for i in range(5):
+        for order in product(alpha, repeat=num):
+            dictionary.append(''.join(order))
+        num+=1
+    
+    dictionary.sort()
+    answer = dictionary.index(word) + 1
+    
     return answer
