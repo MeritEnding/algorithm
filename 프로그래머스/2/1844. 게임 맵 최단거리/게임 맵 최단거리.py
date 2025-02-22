daca3f776807fa383d7fa0d7893ff63562291f6c
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-22 13:00:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -9,24 +9,25 @@ def solution(maps):
     def bfs(x,y):
         queue =deque()
         queue.append((x,y))
+        
         while queue:
-            x,y =queue.popleft()
-
+            x,y=queue.popleft()
             for i in range(4):
                 nx =x+dx[i]
-                ny =y +dy[i]
-
+                ny =y+dy[i]
+                
                 if nx<0 or ny<0 or nx>=n or ny>=m:
                     continue
                 if maps[nx][ny]==0:
                     continue
                 if maps[nx][ny]==1:
-                    maps[nx][ny] =maps[x][y]+1
+                    maps[nx][ny]=maps[x][y]+1
                     queue.append((nx,ny))
         return maps[n-1][m-1]
-            
     
-    answer =bfs(0,0)
+    
+    
+    answer = bfs(0, 0)
     
     if answer ==1:
         return -1
