1ee33683db731d391fdaa70902d49351a4911796
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-25 21:38:17 +0900

    [Silver IV] Title: 로프, Time: 2576 ms, Memory: 40120 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+rope =[]
+
+for i in range(n):
+    rope.append(int(input()))
+
+rope.sort(reverse=True)
+
+result =[]
+
+for j in range(n):
+    result.append(rope[j]*(j+1))
+
+print(max(result))
+
+
+
