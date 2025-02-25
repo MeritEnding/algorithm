00196ab87d1e70d760324ca31bf4c7a74fe03a55
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2025-02-25 21:10:04 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,12 +1,14 @@
 math = input().split('-')
 
 arr=[]
+
 for i in math:
     temp = list(map(int, i.split('+')))
     arr.append(sum(temp))
 
-result =arr[0]
+result=arr[0]
+
 for i in arr[1:]:
-    result -= i
+    result -=i
 
 print(result)
