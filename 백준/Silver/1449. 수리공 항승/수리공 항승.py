b46482abec478be7f1b27e3662bae943a9cda247
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-02-26 15:41:38 +0900

    [Silver III] Title: 수리공 항승, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -1,15 +1,13 @@
 n, l = map(int,input().split())
+location = list(map(int, input().split()))
 
-arr= list(map(int,input().split()))
-
-arr.sort()
-
-start =0
-cnt =0
-
-for i in arr:
-    if start< i:
+location.sort()
+start =0 
+count =0
+for i in location:
+    if start < i:
+        count+=1
         start = i+l-1
-        cnt+=1
 
-print(cnt)
+print(count)
+
