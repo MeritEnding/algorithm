3bb63095c036f6d218f35fb2a30a2bf70f73c38c
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-02-26 14:08:47 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 564 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -0,0 +1,13 @@
+n, m = map(int,input().split())
+arr =list(map(int,input().split()))
+
+for i in range(m):
+    a=min(arr)
+    arr.remove(a)
+    b=min(arr)
+    arr.remove(b)
+    for i in range(2):
+        arr.append(a+b)
+    
+
+print(sum(arr))
