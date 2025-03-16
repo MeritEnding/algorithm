cc7cde5557e27662d18184febda99c78e1ea2639
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-03-16 23:21:38 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 9.63 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,10 @@
 def solution(nums):
+    
     n_dict={}
     for num in nums:
-        n_dict[num]=1
+        n_dict[num] = 1
 
-    if len(nums)//2 <len(n_dict):
+    if len(nums) // 2 < len(n_dict):
         return len(nums)//2
     else:
         return len(n_dict)
