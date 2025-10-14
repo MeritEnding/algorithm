927e7f0a453f7a50167a71ec1d2401c5c25a72fa
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-10-14 23:14:19 +0900

    [Gold V] Title: 회의실 배정, Time: 2732 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,20 +1,19 @@
 n = int(input())
-meetings = []
+meetings =[]
 result = 0
 
 for i in range(n):
-    meetings.append(list(map(int,input().split())))
+    meetings.append(list(map(int, input().split())))
 
-meetings.sort()
-
+meetings.sort(key = lambda x: (x[1], x[0]))
 temp = meetings[0]
 
-for i in range(1,n):
+for i in range(1, n):
     if temp[1] <= meetings[i][0]:
         result+=1
-        temp =meetings[i]
-    elif temp[1]>meetings[i][1]:
-        temp= meetings[i]
-result+=1
+        temp = meetings[i]
+    else:
+        continue
+result += 1
 
 print(result)
