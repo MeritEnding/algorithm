25f6461d9ebc026d836b956869d9c50c2b36aef5
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-10-15 23:19:10 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,27 @@
-n =int(input())
+
+n = int(input())
 m =int(input())
+answer=[]
 
-
-visited=[False]*(n+1)
 graph=[[] for i in range(n+1)]
 
+visited=[False]*(n+1)
+
 for _ in range(m):
-    a,b= map(int,input().split())
+    a,b =map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
+
 def dfs(graph,v,visited):
-    visited[v]= True
-    count = 1
+    visited[v]=True
+    count =1
 
     for i in graph[v]:
         if not visited[i]:
-            
             count +=dfs(graph,i,visited)
+            
     return count
 
-print(dfs(graph, 1, visited)-1)
+
+print(dfs(graph, 1,visited)-1)
