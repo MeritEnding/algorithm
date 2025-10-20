afd09aeb20a6e59b81efa1e0b39fd48e1467b18e
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-10-20 23:29:03 +0900

    [Silver II] Title: 유기농 배추, Time: 200 ms, Memory: 32712 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,35 +1,33 @@
 import sys
-sys.setrecursionlimit(10000)
 
+sys.setrecursionlimit(10000)
 t = int(input())
 
+
 for _ in range(t):
+    m, n, k = map(int, input().split()) 
     count = 0
-    m, n, k = map(int, input().split())
-    graph =[[0]*(n) for i in range(m)]
+    graph =[[0]*n for i in range(m)]
 
     for i in range(k):
         a, b = map(int, input().split())
         graph[a][b] = 1
-
-
-    def dfs(x, y):    
-
-        if x<0 or y<0 or x>=m or y>=n:
+    
+    def dfs(x, y):
+        if x < 0 or y < 0 or x>=m or y>=n:
             return False
-        
         if graph[x][y] == 1:
-            graph[x][y] =0
+            graph[x][y] = 0
             dfs(x-1, y)
+            dfs(x,y-1)
             dfs(x+1, y)
-            dfs(x, y-1)
-            dfs(x, y+1)
+            dfs(x,y+1)
             return True
         return False
 
     for i in range(m):
         for j in range(n):
-            if dfs(i, j) == True:
+            if dfs(i,j) == True:
                 count+=1
 
     print(count)
